06e1be0445bb455d00c79998b3340c173632b463
ngarawakimani <dancankimani70@gmail.com>
2020-08-08 12:08:02 +0300

    updated pesapal callbacks

--- src/JTech/PesapalListener.php
+++ src/JTech/PesapalListener.php
@@ -9,10 +9,12 @@ use Illuminate\Http\Request;
 class PesapalListener {
 
     private $consumer_key;
-
     private $consumer_secret;
-
     private $statusrequestAPI;
+    private $pesapal_transaction_tracking_id;
+    private $pesapal_merchant_reference;
+    private $pesapalNotification="CHANGE";
+    private $pesapal_notification_type;
 
     public function __construct() {
         $this->consumer_key = Config::get('pesapal.auth.consumer_key');
@@ -22,17 +24,26 @@ class PesapalListener {
             $this->statusrequestAPI = Config::get('pesapal.auth.demo_statusrequestAPI');
         } elseif (\App::environment('production')) {
             $this->statusrequestAPI = Config::get('pesapal.auth.live_statusrequestAPI');
-        }
+       }
     }
 
     public function response(Request $request){
-        //dd($request);
         //$pesapal_notification_type        = $request->get('pesapal_notification_type');
-        $pesapalNotification="CHANGE";
-        $pesapal_transaction_tracking_id  = $request->get('pesapal_transaction_tracking_id');
-        $pesapal_merchant_reference       = $request->get('pesapal_merchant_reference');
+        $this->pesapal_transaction_tracking_id  = $request->get('pesapal_transaction_tracking_id');
+        $this->pesapal_merchant_reference       = $request->get('pesapal_merchant_reference');
+    }
 
-        if ($pesapal_transaction_tracking_id != '') {
+    public function statusChecker($data){
+        //$pesapal_notification_type        = $data['pesapal_notification_type'];
+        $this->pesapal_transaction_tracking_id  = $data['pesapal_transaction_tracking_id'];
+        $this->pesapal_merchant_reference       = $data['pesapal_merchant_reference'];
+
+        return $this->sendRequest();
+    }
+
+    private function sendRequest(){
+
+        if ($this->pesapal_transaction_tracking_id != '') {
 
             $token = $params = NULL;
             $consumer = new \OAuthConsumer($this->consumer_key, $this->consumer_secret);
@@ -40,8 +51,8 @@ class PesapalListener {
             // Get transaction status
             $signature_method = new \OAuthSignatureMethod_HMAC_SHA1();
             $request_status = \OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $this->statusrequestAPI, $params);
-            $request_status -> set_parameter('pesapal_merchant_reference', $pesapal_merchant_reference);
-            $request_status -> set_parameter('pesapal_transaction_tracking_id',$pesapal_transaction_tracking_id);
+            $request_status -> set_parameter('pesapal_merchant_reference', $this->pesapal_merchant_reference);
+            $request_status -> set_parameter('pesapal_transaction_tracking_id',$this->pesapal_transaction_tracking_id);
             $request_status -> sign_request($signature_method, $consumer, $token);
 
             $ch = curl_init();
@@ -68,7 +79,7 @@ class PesapalListener {
 
            // Transaction status
            $elements = preg_split('/=/',substr($response, $header_size));
-           $status = $elements[1]; // PENDING, COMPLETED or FAILED
+           $responseData = explode(',', $elements[1]);
 
            curl_close ($ch);
 
@@ -79,13 +90,14 @@ class PesapalListener {
             // retry a number of times, if they don't receive the correct response (for
             // example due to network failure). So if successful, we update our DB ...
             // if it FAILED, we can cancel out transaction in our DB and notify user
+
             return [
-                'pesapal_transaction_tracking_id' => $pesapal_transaction_tracking_id,
-                'pesapal_merchant_reference' => $pesapal_merchant_reference,
-                'status' => $status,
+                'pesapal_transaction_tracking_id' => $this->pesapal_transaction_tracking_id,
+                'pesapal_merchant_reference' => $this->pesapal_merchant_reference,
+                'status' => $responseData[2],
+                'method' => $responseData[1]
             ];
         }
-
     }
 
 }
